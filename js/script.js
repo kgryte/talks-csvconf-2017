431cb914e666b5bd8d57911ef60ac62af4b3cec8
kgryte <kgryte@gmail.com>
2017-04-30 17:24:23 -0700

    Add slides

--- js/script.js
+++ js/script.js
@@ -3,5 +3,29 @@
 (function() {
 	'use strict';
 
+	var header;
+	var footer;
+	var banner;
 
+	/**
+	* Callback invoked upon a `slidechanged` event.
+	*
+	* @private
+	* @param {Event} event - event
+	*/
+	function onChange( event ) {
+		if ( event.currentSlide === banner ) {
+			header.classList.add( 'hidden' );
+			footer.classList.add( 'hidden' );
+		} else if ( event.previousSlide === banner ) {
+			header.classList.remove( 'hidden' );
+			footer.classList.remove( 'hidden' );
+		}
+	}
+
+	header = document.querySelector( 'header' );
+	footer = document.querySelector( 'footer' );
+	banner = document.querySelector( '#banner' );
+
+	Reveal.addEventListener( 'slidechanged', onChange );
 })();
