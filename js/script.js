1d9bee4066304c933e5486e8a249a383db6f8695
kgryte <kgryte@gmail.com>
2017-04-30 22:44:20 -0700

    Hide header and footer for splash slide

--- js/script.js
+++ js/script.js
@@ -3,9 +3,29 @@
 (function() {
 	'use strict';
 
-	var header;
-	var footer;
-	var banner;
+	var header = document.querySelector( 'header' );
+	var footer = document.querySelector( 'footer' );
+	var splash = document.querySelector( '#splash' );
+	var banner = document.querySelector( '#banner' );
+
+	/**
+	* Callback invoked upon a `ready` event.
+	*
+	* @private
+	* @param {Event} event - event
+	*/
+	function onReady( event ) {
+		if (
+			event.currentSlide === banner ||
+			event.currentSlide === splash
+		) {
+			header.classList.add( 'hidden' );
+			footer.classList.add( 'hidden' );
+		} else {
+			header.classList.remove( 'hidden' );
+			footer.classList.remove( 'hidden' );
+		}
+	}
 
 	/**
 	* Callback invoked upon a `slidechanged` event.
@@ -14,18 +34,21 @@
 	* @param {Event} event - event
 	*/
 	function onChange( event ) {
-		if ( event.currentSlide === banner ) {
+		if (
+			event.currentSlide === banner ||
+			event.currentSlide === splash
+		) {
 			header.classList.add( 'hidden' );
 			footer.classList.add( 'hidden' );
-		} else if ( event.previousSlide === banner ) {
+		} else if (
+			event.previousSlide === banner ||
+			event.previousSlide === splash
+		) {
 			header.classList.remove( 'hidden' );
 			footer.classList.remove( 'hidden' );
 		}
 	}
 
-	header = document.querySelector( 'header' );
-	footer = document.querySelector( 'footer' );
-	banner = document.querySelector( '#banner' );
-
+	Reveal.addEventListener( 'ready', onReady );
 	Reveal.addEventListener( 'slidechanged', onChange );
 })();
